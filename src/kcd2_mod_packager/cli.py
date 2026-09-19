271f324880aeb1e9646841d734bdf1f43727768d
ddbb07 <50288525+ddbb07@users.noreply.github.com>
2026-09-19 04:35:35 +0200

    Add path through arguments

--- src/kcd2_mod_packager/cli.py
+++ src/kcd2_mod_packager/cli.py
@@ -1,10 +1,26 @@
+import argparse
 from pathlib import Path
+
+from pydantic import BaseModel, ConfigDict
 
 from kcd2_mod_packager.lib import MakePackage
 
 
+class Args(BaseModel):
+    model_config = ConfigDict(strict=True)
+
+    package_path: Path
+
+
+def parse_args() -> Args:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("package_path", type=Path, help="Path to the package")
+    return Args.model_validate(vars(parser.parse_args()))
+
+
 def main() -> None:
-    with MakePackage(Path.cwd()) as mkpkg:
+    args = parse_args()
+    with MakePackage(args.package_path.absolute()) as mkpkg:
         mkpkg.make_package()
 
 
